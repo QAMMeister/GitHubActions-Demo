1d900f53bd53fef192e3ef1325c12beb7a90cbe7
Arnd Vorrath <73040011+ArndVorrath@users.noreply.github.com>
2024-02-03 21:48:09 +0100

    Create HelloWorldSteps.java

---
+++ src/test/java/StepDefinitions/HelloWorldSteps.java
@@ -0,0 +1,28 @@
+package StepDefinitions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static org.junit.Assert.assertTrue;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class HelloWorldSteps {
+    private WebDriver driver;
+
+    @Given("I open the URL {string}")
+    public void iOpenTheURL(String url) {
+    	FirefoxOptions options = new FirefoxOptions();
+    	options.setBinary("/usr/lib/firefox/firefox");
+    	driver = new FirefoxDriver(options);
+        driver.get(url);
+    }
+
+    @Then("I should see the text {string}")
+    public void iShouldSeeTheText(String expectedText) {
+        String pageSource = driver.getPageSource();
+        assertTrue("The text \"" + expectedText + "\" was not found on the page.", pageSource.contains(expectedText));
+        driver.quit();
+    }
+}
