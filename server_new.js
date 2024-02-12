240af4e0f9ccd4b84c9e169318704d4b9f958c48
Arnd Vorrath <73040011+ArndVorrath@users.noreply.github.com>
2024-02-12 19:14:20 +0100

    Update server_new.js

--- server_new.js
+++ server_new.js
@@ -22,3 +22,13 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+
+var express = require("express");
+var app = express();
+app.get("/url", (req, res, next) => {
+ res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
