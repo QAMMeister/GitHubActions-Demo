2f6c799a6fd973da9cd8ec7e6a234e5ed77b7482
Arnd Vorrath <73040011+ArndVorrath@users.noreply.github.com>
2024-02-24 21:18:21 +0100

    Create TestRunner.java

---
+++ Selenium/src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,14 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Selenium/src/test/resources/Features", 
+                 glue = { "StepDefinitions" },
+                 plugin = {"json:target/cucumber.json"})
+public class TestRunner {
+
+}
